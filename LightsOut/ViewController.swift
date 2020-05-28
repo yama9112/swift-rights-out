87e22f4b6a3bc0b8993dcf61d16f9deeb94ba20f
yama9112 <34400127+yama9112@users.noreply.github.com>
2020-05-28 10:31:26 +0900

    Initial Commit

---
+++ LightsOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightsOut
+//
+//  Created by user1 on 2020/05/28.
+//  Copyright © 2020 yama9112. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
